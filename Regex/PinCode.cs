b7d153b75b612c78c241197617f64a9b588da7d9
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-08-01 17:05:24 +0530

    [Narenthra Kishore] Add :new Solution Pincode validation

---
+++ Regex/PinCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regexs
+{
+    internal class PinCode
+    {
+        public PinCode() 
+        {
+            string regex = "^[0-9]{6}$";
+            Console.WriteLine("Enter the pin code");
+            string input = Console.ReadLine();
+            Regex re = new Regex(regex);
+            if (re.IsMatch(input))
+                Console.WriteLine("Valid pin code");
+            else
+                Console.WriteLine("Pin code not valid");
+        }
+    }
+}
--- Regex/Program.cs
+++ Regex/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Diagnostics.SymbolStore;
 
-namespace Regex
+namespace Regexs
 {
     class Program
     {
         static void Main(string[] args)
         {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.pincode");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice) 
+                {
+                    case 1:
+                        PinCode pinCode = new PinCode();
+                        break;
+                }
 
+            }
         }
     }
 }
